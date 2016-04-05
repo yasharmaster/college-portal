a556560b63266fd3c7524ab4665261b71eff2823
Yash Sharma <ihm2014006@iiita.ac.in>
2016-04-05 07:54:31 +0000

    Add user and post assocication and post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+    belongs_to :user
+    validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+    validates :category, presence: true, length: { minimum: 3, maximum: 10 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+    validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :posts, dependent: :destroy
   validates :roll_number, presence: true, length: { maximum: 10 },
           uniqueness: { case_sensitive: false }
 end
